bc75c1a28712fa4c41eb1bfcde68ecc0ce42eaa1
Sachin Neravath <sachi77n@gmail.com>
2021-08-24 14:35:47 +0530

    Add basic getSelector method

--- methods/vanilla-js-methods.js
+++ methods/vanilla-js-methods.js
@@ -7,6 +7,34 @@ export class Utils {
 
     /* $$ Template START $$ */
 
+    getFirstEl() {
+        if (this.elements && this.elements.length !== undefined) {
+            return this.elements[0];
+        }
+        return this.elements;
+    }
+    static getIdFromSelector(selector) {
+        const selectors = selector.split(' ');
+        const lastSelector = selectors[selectors.length - 1];
+        const fl = lastSelector.substring(0, 1);
+        if (fl === '#') {
+            return lastSelector.substring(1);
+        }
+    }
+
+    static getSelector(selector, context) {
+        if (typeof selector !== 'string') {
+            return selector;
+        }
+        context = context || document;
+
+        // For performance reasons, use getElementById
+        const id = Utils.getIdFromSelector(selector);
+        if (id) {
+            return document.getElementById(id);
+        }
+        return context.querySelectorAll(selector);
+    }
     /* $$ Template END $$ */
 }
 
