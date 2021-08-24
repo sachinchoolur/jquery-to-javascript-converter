1c920c602fd2a6128172311ba0d81f714f827068
Sachin Neravath <sachi77n@gmail.com>
2021-08-24 14:27:51 +0530

    Setup build tools

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "testEnvironment": "jsdom",
+    "coverageReporters": ["json-summary"]
+}
---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import { babel } from '@rollup/plugin-babel';
+
+const config = {
+  input: 'methods/vanilla-js-methods.js',
+  output: {
+    dir: 'demo/output',
+    name: '$utils',
+    format: 'umd'
+  },
+    watch: {
+        include: 'methods/**',
+    },
+  plugins: [babel({ babelHelpers: 'bundled' })]
+};
+
+export default config;
