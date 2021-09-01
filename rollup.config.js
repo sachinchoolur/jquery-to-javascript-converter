b78fed7eb94ba489961fb3b90089a5c01a20637b
Sachin Neravath <sachi77n@gmail.com>
2021-09-01 13:05:11 +0530

    Add build:watch script

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
 import { babel } from '@rollup/plugin-babel';
 
 const config = {
-  input: 'methods/vanilla-js-methods.js',
-  output: {
-    dir: 'demo/output',
-    name: '$utils',
-    format: 'umd'
-  },
+    input: 'methods/vanilla-js-methods.js',
+    output: {
+        dir: 'demo/output',
+        name: '$utils',
+        format: 'umd',
+    },
     watch: {
         include: 'methods/**',
     },
-  plugins: [babel({ babelHelpers: 'bundled' })]
+    plugins: [babel({ babelHelpers: 'bundled' })],
 };
 
 export default config;
